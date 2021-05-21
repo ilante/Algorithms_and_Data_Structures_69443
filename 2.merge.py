3d551e5e3bfe9762ea5f0752e57c00053ec8fa5f
ilante <ila.engl@protonmail.com>
2021-05-21 16:02:20 +0200

    merge ch 2

---
+++ 2.merge.py
@@ -0,0 +1,34 @@
+#!/anaconda3/bin/python3
+
+import math
+import argparse
+
+# For now only MERGE slides ch 2 -- Im defining q q and ar WITHIN the function
+# But for MERGE_SORT p,q and r are defined as parameters!
+
+def merge(ar):
+    '''
+    Takes as input an array. When splitting the array into half, the left
+    part will be longer by one if not divisible by 2. These subarrays will be
+    called left and right. Each of the subarrays is sorted. Merge then merges
+    These sorted arrays into one sorted array. The sorted array is returned.
+    '''
+    p=0 # for now defining always as 0
+    if len(ar)%2==0:
+        q=len(ar)//2-1
+    else:
+        q=len(ar)//2 # BS: indexing starts at 0 math.ceil(ar/2) # Gausche aufrundungsfunktion
+    r=len(ar)
+    print('p', p, 'q', q, 'r', r)
+
+#############################################################################################################################
+# Adding parser
+#############################################################################################################################
+parser = argparse.ArgumentParser(description='MERGE algorithm from ch 2')
+parser.add_argument('-a', '--array', type=list, metavar='', required=True, help='One List of integers composed of 2 sorted halves. Sorting must start from smallest to largest for each of the halves.')
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    a = args.array
+    merge(a)
+
